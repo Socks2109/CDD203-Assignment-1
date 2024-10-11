a39ad15d184dc84b7c20f6643db392023398e82e
Socks2109 <isaacyeohcj@gmail.com>
2024-10-11 00:33:27 -0700

    made changes to DNA RNA transcription

--- seqparser/seq.py
+++ seqparser/seq.py
@@ -6,10 +6,22 @@ def transcribe(seq: str) -> str:
     TODO: transcribes DNA to RNA by generating
     the complement sequence with T -> U replacement
     """
-
+    result = ""
+    for i in seq:
+        if (i == "A"):
+            result += "U"
+        elif (i == "C"):
+            result += "G"
+        elif (i == "T"):
+            result += "A"
+        else:
+            result += "C"
+    return result
 
 def reverse_transcribe(seq: str) -> str:
     """
     TODO: transcribes DNA to RNA then reverses
     the strand
     """
+    result = transcribe(seq)
+    return result[::-1]
